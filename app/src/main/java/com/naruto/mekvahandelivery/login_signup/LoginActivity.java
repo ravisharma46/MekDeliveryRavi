961f4ac51a2bfc33e6deb7a423ef11cc240966a4
amankriet <amankriet@bitbucket.org>
2019-07-31 15:52:36 +0530

    amankriet

--- app/src/main/java/com/naruto/mekvahandelivery/login_signup/LoginActivity.java
+++ app/src/main/java/com/naruto/mekvahandelivery/login_signup/LoginActivity.java
@@ -1,39 +1,19 @@
 package com.naruto.mekvahandelivery.login_signup;
 
-import android.app.ProgressDialog;
-import android.content.Intent;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.DefaultRetryPolicy;
-import com.android.volley.Request;
-import com.android.volley.toolbox.StringRequest;
-import com.naruto.mekvahandelivery.CommonFiles.LoginSessionManager;
-import com.naruto.mekvahandelivery.CommonFiles.MySingleton;
-import com.naruto.mekvahandelivery.NavActivity;
 import com.naruto.mekvahandelivery.R;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static com.naruto.mekvahandelivery.CommonFiles.CommonVaribalesFunctions.NO_OF_RETRY;
-import static com.naruto.mekvahandelivery.CommonFiles.CommonVaribalesFunctions.RETRY_SECONDS;
 
 public class LoginActivity extends AppCompatActivity {
 

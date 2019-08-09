b49e8a45f6f8dc9a924d448bc21d63d08ece7eec
RAVI SHARMA <ravi.ravi.sharma46@gmail.com>
2019-08-09 17:31:55 +0530

    Update OngoingBookingCustomerDrop.java

--- app/src/main/java/com/naruto/mekvahandelivery/vendor_pickup/OngoingBookingCustomerDrop.java
+++ app/src/main/java/com/naruto/mekvahandelivery/vendor_pickup/OngoingBookingCustomerDrop.java
@@ -12,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -19,6 +20,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.naruto.mekvahandelivery.R;
 import static com.naruto.mekvahandelivery.CommonFiles.CommonVaribalesFunctions.dropConfirm;
+import static com.naruto.mekvahandelivery.CommonFiles.CommonVaribalesFunctions.callIntent;
 import static com.naruto.mekvahandelivery.CommonFiles.CommonVaribalesFunctions.sendNavigateIntent;
 import com.naruto.mekvahandelivery.customer_report.AddCustomerReport;
 import com.naruto.mekvahandelivery.customer_report.ViewCustomerReport;
@@ -28,6 +30,9 @@ public class OngoingBookingCustomerDrop extends AppCompatActivity {
     private Button btadd_report,bt_drop;
     private LinearLayout navigation;
     private EditText otp;
+    private ImageView call;
+    private TextView sub_total,tax,additional_charge,total,nameC,addressC,dateD,timeD;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +41,17 @@ public class OngoingBookingCustomerDrop extends AppCompatActivity {
         btadd_report= findViewById(R.id.bt_addReport);
         bt_drop=findViewById(R.id.bt_drop);
         navigation=findViewById(R.id.ll_navigation);
+        call=findViewById(R.id.call);
+
+        sub_total=findViewById(R.id.tv_subtotal);
+        tax=findViewById(R.id.tv_tax);
+        additional_charge=findViewById(R.id.tv_additonalcharge);
+        total=findViewById(R.id.tv_total);
+        nameC=findViewById(R.id.tv_name);
+        addressC=findViewById(R.id.tv_address);
+        dateD=findViewById(R.id.tv_date);
+        timeD=findViewById(R.id.tv_time);
+
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffffff")));
@@ -44,13 +60,44 @@ public class OngoingBookingCustomerDrop extends AppCompatActivity {
         upArrow.setColorFilter(getResources().getColor(R.color.chart_deep_red), PorterDuff.Mode.SRC_ATOP);
         getSupportActionBar().setHomeAsUpIndicator(upArrow);
 
+
+
+        Bundle bundle=getIntent().getExtras();
+        String name =bundle.getString("name");
+        String address =bundle.getString("address");
+        double latitude= Double.parseDouble(bundle.getString("latitude"));
+        double longitude=Double.parseDouble( bundle.getString("longitude"));
+        String dropdate=bundle.getString("dropDate");
+        String dropTime= bundle.getString("dropTime");
+        String amount=bundle.getString("amount");
+        String otp_1=bundle.getString("otp");
+        String mobileNo=bundle.getString("mobile");
+
+
         //OTP text
         otp=findViewById(R.id.et_otp);
         String input = String.valueOf(otp.getText());
 
+        tax.setText("\u20B9" + " 00");
+        additional_charge.setText("\u20B9" + " 00");
+        sub_total.setText("\u20B9 " +amount );
+        total.setText("\u20B9 " + amount);
+        nameC.setText( name);
+        addressC.setText( address);
+        dateD.setText( dropdate);
+        timeD.setText(dropTime);
 
 
 
+
+
+
+       call.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               callIntent(getApplicationContext(),mobileNo);
+           }
+       });
 
 
 
@@ -75,7 +122,7 @@ public class OngoingBookingCustomerDrop extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                sendNavigateIntent(OngoingBookingCustomerDrop.this,28.717010,77.102364);
+                sendNavigateIntent(OngoingBookingCustomerDrop.this,latitude,longitude);
             }
         });
 

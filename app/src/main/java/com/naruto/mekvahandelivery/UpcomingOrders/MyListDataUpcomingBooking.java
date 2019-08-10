0396d9ee58cd00fdb84ae7f2716e73a9b58212fb
RAVI SHARMA <ravi.ravi.sharma46@gmail.com>
2019-08-10 22:51:14 +0530

    Update MyListDataUpcomingBooking.java

--- app/src/main/java/com/naruto/mekvahandelivery/UpcomingOrders/MyListDataUpcomingBooking.java
+++ app/src/main/java/com/naruto/mekvahandelivery/UpcomingOrders/MyListDataUpcomingBooking.java
@@ -12,6 +12,21 @@ public class MyListDataUpcomingBooking {
     private String numberPlate;
     private String mobile;
     private String image_url,otp,name,address,latitude,longitude,drop_date,drop_time,amount;
+    private String action1;
+    private String action2;
+    private String action3;
+    private String action4;
+    private String action5;
+    private String action6;
+    private String action7;
+    private String action8;
+    private String action9;
+    private String action10;
+    private String action11;
+    private String action12;
+    private String action13;
+    private String action14;
+    private String action15;
 
 
     public MyListDataUpcomingBooking(String status,
@@ -24,7 +39,10 @@ public class MyListDataUpcomingBooking {
                                      String orderId,
                                      String payment_status,
                                      String service_name,
-                                     String image_url,String otp,String name,String address,String latitude,String longitude,String drop_date,String drop_time,String amount) {
+                                     String image_url,String otp,String name,String address,String latitude,
+                                     String longitude,String drop_date,String drop_time,String amount,String  vehicleBrand,String service_type,String action1, String action2, String action3,
+                                     String action4, String action5, String action6, String action7, String action8, String action9,
+                                     String action10, String action11, String action12, String action13, String action14, String action15) {
         this.status = status;
         this.date=date;
         this.time=time;
@@ -44,7 +62,81 @@ public class MyListDataUpcomingBooking {
         this.drop_date= drop_date;
         this.drop_time= drop_time;
         this.amount= amount;
+        this.action1 = action1;
+        this.action2 = action2;
+        this.action3 = action3;
+        this.action4 = action4;
+        this.action5 = action5;
+        this.action6 = action6;
+        this.action7 = action7;
+        this.action8 = action8;
+        this.action9 = action9;
+        this.action10 = action10;
+        this.action11 = action11;
+        this.action12 = action12;
+        this.action13 = action13;
+        this.action14 = action14;
+        this.action15 = action15;
 
+    }
+    public String getAction1() {
+        return action1;
+    }
+
+    public String getAction2() {
+        return action2;
+    }
+
+    public String getAction3() {
+        return action3;
+    }
+
+    public String getAction4() {
+        return action4;
+    }
+
+    public String getAction5() {
+        return action5;
+    }
+
+    public String getAction6() {
+        return action6;
+    }
+
+    public String getAction7() {
+        return action7;
+    }
+
+    public String getAction8() {
+        return action8;
+    }
+
+    public String getAction9() {
+        return action9;
+    }
+
+    public String getAction10() {
+        return action10;
+    }
+
+    public String getAction11() {
+        return action11;
+    }
+
+    public String getAction12() {
+        return action12;
+    }
+
+    public String getAction13() {
+        return action13;
+    }
+
+    public String getAction14() {
+        return action14;
+    }
+
+    public String getAction15() {
+        return action15;
     }
 
     public String getStatus() {

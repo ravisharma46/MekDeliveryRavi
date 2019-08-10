68aee9de332792cb55e7e74bc4178b43facd5ba7
RAVI SHARMA <ravi.ravi.sharma46@gmail.com>
2019-08-10 22:50:59 +0530

    Update OnGoingBookingVendorDrop.java

--- app/src/main/java/com/naruto/mekvahandelivery/customer_pickup/OnGoingBookingVendorDrop.java
+++ app/src/main/java/com/naruto/mekvahandelivery/customer_pickup/OnGoingBookingVendorDrop.java
@@ -4,55 +4,61 @@ package com.naruto.mekvahandelivery.customer_pickup;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Matrix;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.ceylonlabs.imageviewpopup.ImagePopup;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
-import com.journeyapps.barcodescanner.BarcodeEncoder;
+import com.naruto.mekvahandelivery.CustomListData.CustomListAdapter;
+import com.naruto.mekvahandelivery.OngoingOrders.MyListDataOngoingBooking;
 import com.naruto.mekvahandelivery.R;
-import com.naruto.mekvahandelivery.customer_report.AddCustomerReport;
 import com.naruto.mekvahandelivery.customer_report.ViewCustomerReport;
-import com.naruto.mekvahandelivery.vendor_pickup.OngoingBookingCustomerDrop;
-import com.naruto.mekvahandelivery.vendor_pickup.UpcomingBookingVendor;
 
 import static com.naruto.mekvahandelivery.CommonFiles.CommonVaribalesFunctions.callIntent;
 import static com.naruto.mekvahandelivery.CommonFiles.CommonVaribalesFunctions.dropConfirm;
-import static com.naruto.mekvahandelivery.CommonFiles.CommonVaribalesFunctions.getFormattedDate;
 import static com.naruto.mekvahandelivery.CommonFiles.CommonVaribalesFunctions.sendNavigateIntent;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class OnGoingBookingVendorDrop extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+
     private LinearLayout paint_linear,navigation;
-    private TextView tvDetails, date, time,name,address,vehicleBrand,vehicleName,numberPlate;
+    private TextView tvDetails, date, time,name,address,vehicleBrand,vehicleName,numberPlate,serviceName;
     private ImageView call,iv_qrcode,vehicle_image;
     private Button report_show,drop;
+
+
+    private List<MyListDataOngoingBooking> mBookingList;
+    public ArrayList<String>arrayList;
+    public ArrayList<String>arrayListsend;
+
 
 
     @Override
@@ -60,7 +66,8 @@ public class OnGoingBookingVendorDrop extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ongoingbooking_vendordrop);
 
-        paint_linear = findViewById(R.id.linear_paint_1);
+
+
         tvDetails = findViewById(R.id.tvDetails_1);
 
         report_show=findViewById(R.id.report_view);
@@ -78,6 +85,11 @@ public class OnGoingBookingVendorDrop extends AppCompatActivity {
         vehicleBrand=findViewById(R.id.tv_vehiclebrand);
         vehicleName=findViewById(R.id.tv_vehiclename);
         numberPlate=findViewById(R.id.tv_numberPlate);
+        serviceName=findViewById(R.id.tv_servicename);
+
+        arrayList=new ArrayList<>();
+        arrayListsend=new ArrayList<>();
+
 
         Bundle bundle=getIntent().getExtras();
         String name_1 =bundle.getString("name");
@@ -93,8 +105,52 @@ public class OnGoingBookingVendorDrop extends AppCompatActivity {
         String vehiclebrand=bundle.getString("vehiclebrand");
         String numberplate=bundle.getString("numberplate");
         String vehicleImageUrl=bundle.getString("imageurl");
+        String servicename=bundle.getString("servicename");
+        String action1=bundle.getString("action1");
+        String action2=bundle.getString("action2");
+        String action3=bundle.getString("action3");
+        String action4=bundle.getString("action4");
+        String action5=bundle.getString("action5");
+        String action6=bundle.getString("action6");
+        String action7=bundle.getString("action7");
+        String action8=bundle.getString("action8");
+        String action9=bundle.getString("action9");
+        String action10=bundle.getString("action10");
+        String action11=bundle.getString("action11");
+        String action12=bundle.getString("action12");
+        String action13=bundle.getString("action13");
+        String action14=bundle.getString("action14");
+        String action15=bundle.getString("action15");
+        arrayList.add(action1);
+        arrayList.add(action2);
+        arrayList.add(action3);
+        arrayList.add(action4);
+        arrayList.add(action6);
+        arrayList.add(action7);
+        arrayList.add(action8);
+        arrayList.add(action9);
+        arrayList.add(action10);
+        arrayList.add(action11);
+        arrayList.add(action12);
+        arrayList.add(action13);
+        arrayList.add(action14);
+        arrayList.add(action15);
+
+        for(int i=0;i<arrayList.size();i++){
+            if(arrayList.get(i).isEmpty()){
+                break;
+            }else{
+                arrayListsend.add(arrayList.get(i));
+            }
+        }
 
 
+
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerView_listView);
+        recyclerView.hasFixedSize();
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplication()));
+        adapter = new CustomListAdapter((ArrayList<String>) arrayListsend);
+        recyclerView.setAdapter(adapter);
 
          name.setText(name_1);
          address.setText(address_1);
@@ -106,6 +162,7 @@ public class OnGoingBookingVendorDrop extends AppCompatActivity {
          vehicleName.setText(vehiclename);
          vehicleBrand.setText(vehiclebrand);
          numberPlate.setText(numberplate);
+         serviceName.setText(servicename);
 
 
 
@@ -127,7 +184,7 @@ public class OnGoingBookingVendorDrop extends AppCompatActivity {
         final ImagePopup imagePopup = new ImagePopup(this);
         imagePopup.setWindowHeight(800); // Optional
         imagePopup.setWindowWidth(800); // Optional
-       imagePopup.setBackgroundColor(getResources().getColor(R.color.offwhite_01));
+        imagePopup.setBackgroundColor(getResources().getColor(R.color.offwhite_01));
         imagePopup.setFullScreen(true); // Optional
         imagePopup.setHideCloseIcon(true);  // Optional
         imagePopup.setImageOnClickClose(true);  // Optional
@@ -177,10 +234,10 @@ public class OnGoingBookingVendorDrop extends AppCompatActivity {
             public void onClick(View view) {
 
                 if (check == 1) {
-                    paint_linear.setVisibility(View.VISIBLE);
+                  recyclerView.setVisibility(View.VISIBLE);
                     check = 0;
                 } else {
-                    paint_linear.setVisibility(View.GONE);
+                   recyclerView.setVisibility(View.GONE);
                     check = 1;
                 }
 

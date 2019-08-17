44212f8d9b9b2db13408a32cbc5ba46657cc34ba
RAVI SHARMA <ravi.ravi.sharma46@gmail.com>
2019-08-17 21:13:02 +0530

    Update UpcomingBookingCustomer.java

--- app/src/main/java/com/naruto/mekvahandelivery/customer_pickup/UpcomingBookingCustomer.java
+++ app/src/main/java/com/naruto/mekvahandelivery/customer_pickup/UpcomingBookingCustomer.java
@@ -1,6 +1,9 @@
 package com.naruto.mekvahandelivery.customer_pickup;
 
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
@@ -9,6 +12,7 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.text.Html;
 import android.util.Log;
@@ -19,30 +23,49 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.toolbox.StringRequest;
 import com.broooapps.otpedittext2.OtpEditText;
 import com.bumptech.glide.Glide;
+import com.naruto.mekvahandelivery.CaptureActivityPortrait;
+import com.naruto.mekvahandelivery.NavActivity;
 import com.naruto.mekvahandelivery.R;
+import com.naruto.mekvahandelivery.common_files.LoginSessionManager;
+import com.naruto.mekvahandelivery.common_files.MySingleton;
 import com.naruto.mekvahandelivery.custom_list_data.CustomListAdapter;
 import com.naruto.mekvahandelivery.customer_report.AddCustomerReport;
+
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
 
+import static com.naruto.mekvahandelivery.common_files.CommonVaribalesFunctions.NO_OF_RETRY;
+import static com.naruto.mekvahandelivery.common_files.CommonVaribalesFunctions.RETRY_SECONDS;
 import static com.naruto.mekvahandelivery.common_files.CommonVaribalesFunctions.callIntent;
 import static com.naruto.mekvahandelivery.common_files.CommonVaribalesFunctions.pickupConfirm;
+import static com.naruto.mekvahandelivery.common_files.LoginSessionManager.ACCESS_TOKEN;
+import static com.naruto.mekvahandelivery.common_files.LoginSessionManager.TOKEN_TYPE;
 
 public class UpcomingBookingCustomer extends AppCompatActivity implements AddUpcomingCustomerPickupAdapter.OnAdapterClickListener {
     private RecyclerView recyclerView, recyclerViewCustPickup;
@@ -58,8 +81,13 @@ public class UpcomingBookingCustomer extends AppCompatActivity implements AddUpc
     private Uri photoURI;
     private int photoIndex = 0;
 
-    private String otp_input="";
+    private String otp_input="",bookingid="";
     private OtpEditText otpEditText;
+
+    private static final String myUrl = "https://mekvahan.com/api/delivery/upcoming_ongoing";
+    private static final String myUrl_img = "https://mekvahan.com/api/delivery/pickup_image";
+
+    private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -99,7 +127,7 @@ public class UpcomingBookingCustomer extends AppCompatActivity implements AddUpc
         assert bundle != null;
         String name_1 =bundle.getString("name");
         String vehicle_type =bundle.getString("vehicletype");
-        String bookingid =bundle.getString("bookingid");
+        bookingid =bundle.getString("bookingid");
         String address_1 =bundle.getString("address");
 //        double latitude= Double.parseDouble(bundle.getString("latitude"));
     //    double longitude=Double.parseDouble( bundle.getString("longitude"));
@@ -156,6 +184,8 @@ public class UpcomingBookingCustomer extends AppCompatActivity implements AddUpc
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplication()));
         adapter = new CustomListAdapter(arrayListsend,"upcoming");
         recyclerView.setAdapter(adapter);
+
+
 
         recyclerViewCustPickup = findViewById(R.id.rv_imagecustomerpickup);
         recyclerViewCustPickup.setHasFixedSize(false);
@@ -227,13 +257,146 @@ public class UpcomingBookingCustomer extends AppCompatActivity implements AddUpc
             @Override
             public void onClick(View view) {
 
-               pickupConfirm(UpcomingBookingCustomer.this);
+                sendDb_pickupConfirm();
+
             }
         });
 
-
+        mProgressDialog = new ProgressDialog(getApplicationContext());
+        mProgressDialog.setMessage("Please wait...");
 
     }
+
+
+    private void sendDb_pickupConfirm(){
+        //mProgressDialog.show();
+        StringRequest stringRequest=new StringRequest(Request.Method.POST,myUrl,response -> {
+
+            try{
+
+                JSONObject object=new JSONObject(response);
+                int status_1 = object.getInt("status");
+                if(status_1!=1) {
+                     dialogpop();
+                    //Toast.makeText(getApplicationContext(),"Incorrect OTP",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                //mProgressDialog.dismiss();
+                pickupConfirm(UpcomingBookingCustomer.this);
+                delay(new NavActivity());
+
+
+
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+
+
+
+
+        },error -> {
+            Toast.makeText(getApplicationContext(),"Something get wrong",Toast.LENGTH_LONG).show();
+            Log.e("TAG", error.toString());
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> headers=new HashMap<>();
+                headers.put("otp",otp_input);
+                return headers;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> headers=new HashMap<>();
+                headers.put("Accept","application/json");
+                LoginSessionManager loginSessionManager=new LoginSessionManager(Objects.requireNonNull(getApplication()));
+                HashMap<String,String> token=loginSessionManager.getUserDetailsFromSP();
+                String token_type=token.get(TOKEN_TYPE);
+                String acces_token= token.get(ACCESS_TOKEN);
+                headers.put("Authorization",token_type+" "+acces_token);
+
+                return headers;
+            }
+        };
+
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy((RETRY_SECONDS*1000),
+                NO_OF_RETRY,DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+        MySingleton.getInstance(getApplicationContext()).addToRequestQueue(stringRequest);
+
+    }
+
+
+
+    private void sendDb_exterior_image(){
+
+        StringRequest stringRequest=new StringRequest(Request.Method.POST,myUrl_img,response -> {
+
+            try{
+
+                JSONObject object=new JSONObject(response);
+                int status_1 = object.getInt("status");
+                if(status_1!=1) {
+                    dialogpop();
+                    //Toast.makeText(getApplicationContext(),"Incorrect OTP",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                sendDb_pickupConfirm();
+
+
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+
+
+
+
+        },error -> {
+            Toast.makeText(getApplicationContext(),"Something get wrong",Toast.LENGTH_LONG).show();
+            Log.e("TAG", error.toString());
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> headers=new HashMap<>();
+                headers.put("pickup_image","pickupimage");
+                String bookingID=bookingid.replace("#","");
+                headers.put("booking_id",bookingID);
+
+                return headers;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> headers=new HashMap<>();
+                headers.put("Accept","application/json");
+                LoginSessionManager loginSessionManager=new LoginSessionManager(Objects.requireNonNull(getApplication()));
+                HashMap<String,String> token=loginSessionManager.getUserDetailsFromSP();
+                String token_type=token.get(TOKEN_TYPE);
+                String acces_token= token.get(ACCESS_TOKEN);
+                headers.put("Authorization",token_type+" "+acces_token);
+
+                return headers;
+            }
+        };
+
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy((RETRY_SECONDS*1000),
+                NO_OF_RETRY,DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+        MySingleton.getInstance(getApplicationContext()).addToRequestQueue(stringRequest);
+
+    }
+
+
+
+
+
+
+
+
+
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -309,4 +472,40 @@ public class UpcomingBookingCustomer extends AppCompatActivity implements AddUpc
         recyclerViewCustPickup.getRecycledViewPool().clear();
         adapterCustPickup.notifyItemRemoved(position);
     }
+
+    private void dialogpop(){
+       // mProgressDialog.dismiss();
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+
+        alertBuilder.setMessage("Incorrect OTP").setCancelable(false);
+
+        alertBuilder.setPositiveButton("Retry", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+
+            }
+        });
+
+
+        AlertDialog alertDialog = alertBuilder.create();
+        alertDialog.setTitle("OTP Results");
+        alertDialog.show();
+    }
+
+    public void delay(Activity activity){
+        Handler handler = new Handler();
+
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(UpcomingBookingCustomer.this, activity.getClass());
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+
+
+            startActivity(intent);
+
+        }, 800);
+    }
+
 }

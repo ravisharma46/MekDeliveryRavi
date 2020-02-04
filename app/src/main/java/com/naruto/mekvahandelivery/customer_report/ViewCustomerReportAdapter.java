f7d1e8515b65485785533dde51738aee3f5fd75f
RAVI SHARMA <ravi.ravi.sharma46@gmail.com>
2020-02-04 12:21:34 +0530

    Update ViewCustomerReportAdapter.java

--- app/src/main/java/com/naruto/mekvahandelivery/customer_report/ViewCustomerReportAdapter.java
+++ app/src/main/java/com/naruto/mekvahandelivery/customer_report/ViewCustomerReportAdapter.java
@@ -1,15 +1,20 @@
 package com.naruto.mekvahandelivery.customer_report;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.ceylonlabs.imageviewpopup.ImagePopup;
 import com.naruto.mekvahandelivery.R;
 
 import java.util.List;
@@ -40,6 +45,7 @@ class ViewCustomerReportAdapter extends RecyclerView.Adapter<ViewCustomerReportA
         return new ViewHolder(view);
     }
 
+    @SuppressLint("ResourceAsColor")
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         final String data = imageStringList.get(position);
@@ -57,6 +63,26 @@ class ViewCustomerReportAdapter extends RecyclerView.Adapter<ViewCustomerReportA
             onReportAdapterClickListener.onReportAdapterInteraction(finalKey,"");
         }
         holder.ivcancel.setVisibility(View.GONE);
+
+        holder.ivevent.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+
+                final Dialog settingsDialog = new Dialog(context);
+                settingsDialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+                settingsDialog.setContentView(R.layout.popup_layout);
+                ImageView image = settingsDialog.findViewById(R.id.chosen_image);
+                Glide.with(context).load(data).fitCenter().into(image);
+                settingsDialog.setCanceledOnTouchOutside(true);
+                settingsDialog.show();
+            }
+
+        });
+
+
+
     }
 
     @Override
@@ -71,11 +97,18 @@ class ViewCustomerReportAdapter extends RecyclerView.Adapter<ViewCustomerReportA
         
         ImageView ivcancel, ivevent;
         
+        @SuppressLint("ResourceAsColor")
         ViewHolder(@NonNull View itemView) {
             super(itemView);
             
             ivcancel = itemView.findViewById(R.id.iv_cancel);
             ivevent = itemView.findViewById(R.id.iv_document);
+
+
+
+
+
+
         }
     }
 
